2b7e93e0b09495191e2b8a824d06d2d4b349685d
Nick Hale <nicholas.hale@gmail.com>
2019-10-08 12:58:57 +0000

    Create adapter for Tynemouth Squash

--- adapters/tynemouth.go
+++ adapters/tynemouth.go
@@ -1,20 +1,22 @@
 package tynemouth
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
-	"io"
 	"net/http"
 	"net/url"
+	"reflect"
 	"strings"
+
+	"github.com/google/go-querystring/query"
 )
 
 const (
-	defaultBaseURL = "http://tynemouth-squash.herokuapp.com/"
-	userAgent      = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36"
+	defaultBaseURL      = "http://tynemouth-squash.herokuapp.com/"
+	defaultBookingsPath = "bookings/new"
+	userAgent           = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36"
 )
 
+// A Client manages communication with the Tynemouth Squash website
 type Client struct {
 	client    *http.Client
 	BaseURL   *url.URL
@@ -25,6 +27,31 @@ type service struct {
 	client *Client
 }
 
+// addOptions adds the parameters in opt as URL query parameters to s.
+// opt must be a struct whose fields may contain "url" tags.
+func addOptions(s string, opt interface{}) (string, error) {
+	v := reflect.ValueOf(opt)
+	if v.Kind() == reflect.Ptr && v.IsNil() {
+		return s, nil
+	}
+
+	u, err := url.Parse(s)
+	if err != nil {
+		return s, err
+	}
+
+	qs, err := query.Values(opt)
+	if err != nil {
+		return s, err
+	}
+
+	u.RawQuery = qs.Encode()
+
+	return u.String(), nil
+}
+
+// NewClient returns a new Tynemouth Squash API client. If a nil httpClient is
+// provided, a new http.Client will be used.
 func NewClient(httpClient *http.Client) *Client {
 	if httpClient == nil {
 		httpClient = &http.Client{}
@@ -35,7 +62,10 @@ func NewClient(httpClient *http.Client) *Client {
 	return c
 }
 
-func (c *Client) NewRequest(method, urlStr string, body interface{}) (*http.Request, error) {
+// NewRequest creates an API request. A relative URL can be provided in urlStr,
+// in which case it is resolved relative to the BaseURL of the Client.
+// Relative URLs should always be specified without a preceding slash.
+func (c *Client) NewRequest(method, urlStr string) (*http.Request, error) {
 	if !strings.HasSuffix(c.BaseURL.Path, "/") {
 		return nil, fmt.Errorf("BaseURL must have a trailing slash, but %q does not", c.BaseURL)
 	}
@@ -44,25 +74,11 @@ func (c *Client) NewRequest(method, urlStr string, body interface{}) (*http.Requ
 		return nil, err
 	}
 
-	var buf io.ReadWriter
-	if body != nil {
-		buf = new(bytes.Buffer)
-		enc := json.NewEncoder(buf)
-		enc.SetEscapeHTML(false)
-		err := enc.Encode(body)
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	req, err := http.NewRequest(method, u.String(), buf)
+	req, err := http.NewRequest(method, u.String(), nil)
 	if err != nil {
 		return nil, err
 	}
 
-	if body != nil {
-		req.Header.Set("Content-Type", "text/html")
-	}
 	req.Header.Set("Accept", "text/html")
 	if c.UserAgent != "" {
 		req.Header.Set("User-Agent", c.UserAgent)
--- adapters/tynemouth_test.go
+++ adapters/tynemouth_test.go
@@ -2,7 +2,7 @@ package tynemouth
 
 import (
 	"fmt"
-	"io/ioutil"
+	"net/http"
 	"testing"
 )
 
@@ -11,23 +11,41 @@ type Slot struct {
 	Date  string `json:"date"`
 }
 
+type BookingInfo struct {
+	Court    string `url:"court"`
+	Days     string `url:"days"`
+	Hour     string `url:"hour"`
+	Min      string `url:"min"`
+	TimeSlot string `url:"timeSlot"`
+}
+
 func TestNewRequest(t *testing.T) {
 	c := NewClient(nil)
 
 	inURL, outURL := "/foo", defaultBaseURL+"foo"
-	inBody, outBody := Slot{Court: "1", Date: "10/10/2019"}, `{"court":"1","date":"10/10/2019"}`+"\n"
 
-	req, _ := c.NewRequest("GET", inURL, inBody)
+	req, _ := c.NewRequest(http.MethodGet, inURL)
 
+	// test that relative URL was expanded
 	if got, want := req.URL.String(), outURL; got != want {
 		t.Errorf("NewRequest(%q) URL is %v, want %v", inURL, got, want)
 	}
-	got, want := req.URL.String(), outURL
-	fmt.Println(got, want)
 
-	// test that body was JSON encoded
-	body, _ := ioutil.ReadAll(req.Body)
-	if got, want := string(body), outBody; got != want {
-		t.Errorf("NewRequest(%q) Body is %v, want %v", inBody, got, want)
+	// test that default user-agent is attaached to the request
+	if got, want := req.Header.Get("User-Agent"), c.UserAgent; got != want {
+		t.Errorf("NewRequest() User-Agent is %v, want %v", got, want)
+	}
+}
+
+func TestBuildUrl(t *testing.T) {
+	info := BookingInfo{Court: "1", Days: "21", Hour: "19", Min: "50", TimeSlot: "17"}
+
+	u := fmt.Sprintf("%v%v/%v", defaultBaseURL, "bookings", "new")
+	s, _ := addOptions(u, info)
+
+	outURL := defaultBaseURL + "bookings/new?court=1&days=21&hour=19&min=50&timeSlot=17"
+
+	if got, want := s, outURL; got != want {
+		t.Errorf("AddOptions() URL is %v, want %v", got, want)
 	}
 }
