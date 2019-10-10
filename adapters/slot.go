b9f2d773bdf540d18419bcda9162fae691255490
Nick Hale <nicholas.hale@gmail.com>
2019-10-10 15:40:15 +0100

    Update the auth_token extract functionality

--- adapters/slot.go
+++ adapters/slot.go
@@ -2,7 +2,10 @@ package tynemouth
 
 import (
 	"fmt"
+	"io"
 	"net/http"
+
+	"golang.org/x/net/html"
 )
 
 type SlotService service
@@ -21,9 +24,60 @@ type Slot struct {
 	Time      string `url:"time,omitempty"`
 }
 
+// Parse the tynemouth squash new booking page. We need to extract
+// the authenticity_token and booking[start_time]
+// TODO investigate whether booking[start_time] could be a given.
 func (s *Slot) Parse(resp *http.Response) {
-	s.AuthToken = "token"
-	s.Time = "19:50"
+	tokenizer := html.NewTokenizer(resp.Body)
+
+	for {
+		tokenType := tokenizer.Next()
+
+		if tokenType == html.ErrorToken {
+			err := tokenizer.Err()
+			if err == io.EOF {
+				break
+			}
+		}
+
+		if tokenType == html.SelfClosingTagToken {
+			token := tokenizer.Token()
+			if "input" == token.Data {
+				ok, authToken := getAttribute(token, "authenticity_token")
+				if ok {
+					s.AuthToken = authToken
+					continue
+				}
+
+				ok, time := getAttribute(token, "booking[start_time]")
+				if ok {
+					s.Time = time
+					continue
+				}
+			}
+		}
+	}
+}
+
+func getAttribute(t html.Token, attrName string) (ok bool, value string) {
+	found := false
+
+	for _, v := range t.Attr {
+		if v.Key == "name" && v.Val == attrName {
+			found = true
+			break
+		}
+	}
+
+	if found {
+		for _, v := range t.Attr {
+			if v.Key == "value" {
+				return true, v.Val
+			}
+		}
+	}
+
+	return
 }
 
 func (s *SlotService) ListSlot(opt *Slot) error {
@@ -38,7 +92,6 @@ func (s *SlotService) ListSlot(opt *Slot) error {
 		return err
 	}
 
-	// slot needs authtoken and time populating in the write method
 	_, err = s.client.Do(req, opt)
 	if err != nil {
 		return err
--- adapters/slot_test.go
+++ adapters/slot_test.go
@@ -13,6 +13,7 @@ func TestListSlot(t *testing.T) {
 
 	slot := &Slot{Court: "1", Days: "20", Hour: "19", Min: "50", TimeSlot: "17"}
 	authToken := "token"
+	time := "2019-10-30 19:50:00 +0000"
 
 	err := client.Slot.ListSlot(slot)
 	if err != nil {
@@ -22,5 +23,10 @@ func TestListSlot(t *testing.T) {
 	// test that relative URL was expanded
 	if got, want := slot.AuthToken, authToken; got != want {
 		t.Errorf("ListSlot() AuthToken is %v, want %v", got, want)
+	}
+
+	// test that time was captured
+	if got, want := slot.Time, time; got != want {
+		t.Errorf("ListSlot() Time is %v, want  %v", got, want)
 	}
 }
